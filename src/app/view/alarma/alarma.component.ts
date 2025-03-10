9a837a42b09c92938a500dceb72c3fe797b0196b
Harol Cisneros <harolcsnrs@gmail.com>
2025-03-10 09:46:06 -0700

    view

---
+++ src/app/view/alarma/alarma.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alarma',
+  imports: [],
+  templateUrl: './alarma.component.html',
+  styleUrl: './alarma.component.css'
+})
+export class AlarmaComponent {
+
+}
